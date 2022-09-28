83f75383985d694a6ac11429a64b58b0b4876d49
emirhan1414 <113902435+emirhan1414@users.noreply.github.com>
2022-09-28 12:07:12 +0300

    Initial commit

---
+++ src/degiskenyas4/Main.java
@@ -0,0 +1,23 @@
+package degiskenyas4;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String ad ="Emirhan ";
+		String soyad="Aykuş ";
+		String yas= "15";
+		String cinsiyet="erkek";
+		double boy = 1.73;
+		boolean gozluk=false;
+		boolean İsShow=true;
+		if (isShow=true) {
+		Systeam.out.println("Ad: "+ ad);
+		Systeam.out.println("Soyad: "+ soyad);
+		Systeam.out.println("Yaş: "+ yas);
+		Systeam.out.println("Boy: "+ Boy);
+        Systeam.out.println("cinsi: "+yas);
+
+
+	}
+
+}
